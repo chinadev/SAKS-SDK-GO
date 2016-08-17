4551711f367b5c75c7e070873b2429653adff5e8
SuperFashi <admin@superfashi.com>
2016-08-17 21:48:16 +0800

    update test

--- sakshat_test.go
+++ sakshat_test.go
@@ -5,10 +5,27 @@ import (
 	"time"
 )
 
+//func TestBuzzer(t *testing.T) {
+//	Buzzer.BeepAction(time.Second / 5, time.Second / 5, 2)
+//}
+
 func TestLEDRow(t *testing.T) {
+	// Test for Index
 	for i := uint(0); i < 8; i++ {
 		LEDRow.OnForIndex(i)
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second)
 		LEDRow.OffForIndex(i)
 	}
+
+	// Test for SetRow
+	LED1 := [8]bool{0, 1, 0, 1, 0, 1, 0, 1}
+	LED2 := [8]bool{1, 0, 1, 0, 1, 0, 1, 0}
+	LEDRow.SetRow(LED1)
+	time.Sleep(time.Second)
+	LEDRow.SetRow(LED2)
+	time.Sleep(time.Second)
+
+	// Test for All
+	LEDRow.On()
+	LEDRow.Off()
 }
